5357e14d4446148083dbd46967a525f7cd4e5c55
Vbj <ijalab1808@gmail.com>
2020-05-31 17:05:36 +0530

    connecting to database

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require('express');
+const connectDb = require('./config/db');
 
 
 const app = express();
+
+//connecting to database
+connectDb();
+
 
 app.use(express.json({extended: false}));
 app.get('/', (req,res) => res.send('Hello world'))
 
 
+
+//port
 const port = process.env.PORT || 8082;
-
-
-
 app.listen(port, () => console.log(`Server running successfully on port ${port}`));
