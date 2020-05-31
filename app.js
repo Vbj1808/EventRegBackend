78a42827ce19905d71f4a13d0d301cf4212bdf82
Vbj <ijalab1808@gmail.com>
2020-05-31 19:57:06 +0530

    modified app.js

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express'),
+      bodyParser = require('body-parser'),
+      passport = require("passport");
+
 const connectDb = require('./config/db');
 
-
 const app = express();
+
+//added login route
+const loginRoute = require("./routes/api/admin/loginRoute");
 
 //Bodyparser middleware
 app.use(bodyParser.urlencoded({ extended: false}));
@@ -16,7 +20,14 @@ connectDb();
 app.use(express.json({extended: false}));
 app.get('/', (req,res) => res.send('Hello world'))
 
+//Passport middleware
+app.use(passport.initialize());
 
+//Passport config
+require("./auth/passport")(passport);
+
+//Routes
+app.use("/api/admin/loginRoute", admin );
 
 //port
 const port = process.env.PORT || 8082;
