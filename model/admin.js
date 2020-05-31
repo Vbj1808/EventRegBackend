d1db8bba00f0050d1239c86dbcb8143c71d0d276
Vbj <ijalab1808@gmail.com>
2020-05-31 17:57:08 +0530

    added admin schema

---
+++ model/admin.js
@@ -0,0 +1,15 @@
+//ADMIN SCHEMA
+const mongoose = require('mongoose');
+
+const AdminSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true 
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}, {timestamps: true });
+
+module.exports = Admin = mongoose.model('admin', AdminSchema);
