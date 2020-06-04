5898f94d6739317f19e5097783de55d693c553a0
Vbj <ijalab1808@gmail.com>
2020-06-04 12:37:13 +0530

    added unique id

--- model/regForm.js
+++ model/regForm.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const EventRegSchema = new mongoose.Schema({
     
-    uniqueId: {
+    uniqueid: {
         type: Number,
         required: true,
         unique: true 
